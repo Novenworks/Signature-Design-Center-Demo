0f7021b48b94d7cae0ba3ec236493ed7676ab8d3
questionzerozx <91362741+questionzerozx@users.noreply.github.com>
2026-09-09 08:25:11 -0700

    Point photography at first-party Wayback originals until binary upload lands

---
+++ lib/assets.ts
@@ -0,0 +1,18 @@
+export const img = {
+  kitchen:
+    "https://web.archive.org/web/20150623201929im_/http://ocremodel.com/wp-content/uploads/2010/07/oc-kitchen-custom-cabinets1.jpg",
+  showroom:
+    "https://web.archive.org/web/20140213213308im_/http://ocremodel.com/wp-content/uploads/2010/07/Signature-Design-1.jpg",
+  bath:
+    "https://web.archive.org/web/20140214095220im_/http://ocremodel.com/wp-content/uploads/2011/07/Signature_Bathroom-Remodel.jpg",
+  vanity:
+    "https://web.archive.org/web/20060715233302im_/http://www.ocremodel.com/blog/uploaded_images/bathroom-vanity3_oc-710324.jpg",
+  stone:
+    "https://web.archive.org/web/20141211022317im_/http://ocremodel.com/wp-content/uploads/2010/07/travertine-flooring1.jpg",
+  granite:
+    "https://web.archive.org/web/20061026071524im_/http://www.ocremodel.com/blog/uploaded_images/granite_marble_los_angeles-746815.jpg",
+  fred: "https://web.archive.org/web/20140212223559im_/http://ocremodel.com/wp-content/uploads/2010/07/Fred.jpg",
+  sam: "https://web.archive.org/web/20140214205759im_/http://ocremodel.com/wp-content/uploads/2010/07/Sam.jpg",
+  jorge: "https://web.archive.org/web/20140215022748im_/http://ocremodel.com/wp-content/uploads/2010/07/Jorge.jpg",
+  ernie: "https://web.archive.org/web/20140215013425im_/http://ocremodel.com/wp-content/uploads/2010/07/Ernie.jpg",
+};
